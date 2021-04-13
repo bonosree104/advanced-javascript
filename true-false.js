a335f6e1a45be04f288eb242521dbbaa8f2617b1
bonosree104 <bonosreeela104@gmail.com>
2021-04-13 06:21:18 +0600

    true-false comitted

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//falsy;
+//0
+//""
+//null
+//undefined
+//false
+
+//truthy
+//"0" , " ", [];
+
+//const age = 0;
+let name = 12;
+console.log(name);
+if(name || name == 0){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
