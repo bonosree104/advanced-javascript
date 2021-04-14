207fad12d3aa930dd2ef8679d49f572c7f34bded
bonosree104 <bonosreeela104@gmail.com>
2021-04-14 10:24:17 +0600

    null vs undefined interview

---
+++ null-undifined.js
@@ -0,0 +1,12 @@
+//let purvi ;
+//console.log(purvi);
+
+function add(num1,num2){
+    console.log(num1 + num2);
+
+}
+const result= add(13,33);
+console.log(result);
+
+// undefined
+// if we miss something in code then it wouldnot be run and see result undefined.
