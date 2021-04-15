6a079a6210a58723b4ab4476f2269311e45161d5
bonosree104 <bonosreeela104@gmail.com>
2021-04-15 08:01:32 +0600

    scope js

---
+++ scope.js
@@ -0,0 +1,11 @@
+let bonus =20;
+function sum(first,second){
+    let result = first+ second +bonus;
+    //console.log(result);
+    return result;
+} 
+
+const output =sum(8,10);
+console.log(output);
+console.log(bonus);
+// here,s make scope
